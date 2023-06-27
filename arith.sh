c3a28f01c34de57b3596cde81969461d81de45e0
Ugo Uche <120284311+Yughox@users.noreply.github.com>
2023-06-26 18:28:33 -0700

    Update arith.sh

--- arith.sh
+++ arith.sh
@@ -6,24 +6,38 @@ y=$2
 
 echo "x=$1, y=$2"
 sleep 2
+
+#addition
 echo "Addition of x and y"
 echo $(($x+$y))
 sleep 2
+
+#subtraction
 echo "Subtraction of x and y"
 echo $(($x-$y))
 sleep 2
+
+#multiplication
 echo "Multiplication of x and y"
 echo $(($x*$y))
 sleep 2
+
+#Division
 echo "Division of x and y"
 echo $(( $x / $y ))
 sleep 2
+
+#Exponential
 echo "Exponentation of x and y"
 echo $(($x**$y))
 sleep 2
+
+#Modular
 echo "Modular Division of x and y"
 echo $(($x%$y))
 sleep 2
+
+#Increment and Decrement
 echo "Incrementing x by 10, then x="
 ((x+=10))
 echo $x
@@ -36,6 +50,9 @@ echo "Multiply x by 2, then x="
 ((x*=2))
 echo $x
 sleep 2
+
+
+#random test 
 echo "Dividing y by 5, y="
 ((y/=5))
 echo $y
