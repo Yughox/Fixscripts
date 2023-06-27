f994ee7bcf6d06427800892252bef63b6ff49653
Ugo Uche <120284311+Yughox@users.noreply.github.com>
2023-06-26 18:30:33 -0700

    Update carrr.sh

--- carrr.sh
+++ carrr.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+#Creating a variable 
 var1=5 
 
+#reassigning var with new details
 Add () {
 	var1=$(($var1 + 10))
 echo $var1
 }
 Add
 
+#checking output (must be the same) 
 var2=$var1
 echo $var2
