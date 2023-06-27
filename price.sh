9f6a9211e2df1db5316c61c414a2ad19a7e6f22e
Ugo Uche <120284311+Yughox@users.noreply.github.com>
2023-06-26 18:26:09 -0700

    Update price.sh

--- price.sh
+++ price.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#Inputing desired amount
 echo "please enter desired amount"
 read amount
 echo "Taking personal details into account ...."
 sleep 5
+#checking eligibility, visual representation added if required.
 echo "Checking eligibility"
 sleep 5
 
@@ -13,4 +15,5 @@ then
 else 
 
 	echo "You didnt make it"
+ #end of if statement
 fi
